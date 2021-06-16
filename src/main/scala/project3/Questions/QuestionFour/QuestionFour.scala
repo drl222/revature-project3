e786772895d09aebdcb825038345183b66795908
almondWilde <human.ipod7@gmail.com>
2021-06-16 14:37:21 -0700

    Update QuestionFour.scala

--- src/main/scala/project3/Questions/QuestionFour/QuestionFour.scala
+++ src/main/scala/project3/Questions/QuestionFour/QuestionFour.scala
@@ -4,6 +4,13 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 
 package object QuestionFour {
   def questionFour(spark : SparkSession, commonCrawlDF : DataFrame) : Unit = {
+    //write an athena query and receive the csv as output
+
+    //load the output csv into a DF
+
+    //use spark.sql to filter the DF
+
+    //.show() our output
 
   }
 }
