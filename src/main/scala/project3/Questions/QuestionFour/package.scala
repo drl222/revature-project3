ac9dd1f0c1ef817eeed1145fd596a4882bff95d7
almondWilde <human.ipod7@gmail.com>
2021-06-16 13:16:25 -0700

    init questionfour
    made QuestionFour package and main function

---
+++ src/main/scala/project3/Questions/QuestionFour/package.scala
@@ -0,0 +1,9 @@
+package project3.Questions
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+package object QuestionFour {
+  def questionFour(spark : SparkSession, commonCrawlDF : DataFrame) : Unit = {
+
+  }
+}
