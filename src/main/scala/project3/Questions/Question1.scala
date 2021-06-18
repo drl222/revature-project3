d69d88a78dda0e45c28a2bb275d077c2f90f1a83
Francisco Aceves <f_ace76@hotmail.com>
2021-06-18 13:22:07 -0700

    starting point

---
+++ src/main/scala/project3/Questions/Question1.scala
@@ -0,0 +1,10 @@
+package project3.Questions
+
+import org.apache.spark.sql.{Dataset, Row}
+
+object Question1 {
+  def getEntryLevelResults(dataDF: Dataset[Row]) = {
+    val entryLevelDF = dataDF.select("Experience","Entry Level")
+
+  }
+}
